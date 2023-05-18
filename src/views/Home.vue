da6198f243a63ed7380dadf74d6a314f0a99968d
George-Gyang <georgegyang3@gmail.com>
2023-05-18 02:22:42 +0100

    modified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,20 +3,26 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <main class="container mt-5">
-    <div class="row">
-      <div class="pt-5">
-        <div class="col-md-6 landing-col pt-3">
-            <h1 class="text-primary text-center">Quizzybyte</h1>
-          <div class="landing">
-            <div class="">
-              <h3 class="text-center fs-2 mb-5">Test your Quiz-Abilty</h3>
-              <RouterLink to="/quiz" class="fs-4">Proceed to Quiz</RouterLink>
-            </div>
-          </div>
+  <main class="container behind mt-5">
+    <div class="rounded-5 landing-col p-5" style="max-width: 600px;">
+      <h1 class="text-primary text-center">Quizzybyte</h1>
+      <div class="landing">
+        <div class="">
+          <h3 class="text-center fs-2 mb-5">Test your Quiz-Abilty</h3>
+          <RouterLink to="/quiz" class="fs-4">Proceed to Quiz</RouterLink>
         </div>
       </div>
     </div>
+    <!-- <div class="col-md-6 p-0">
+      <div>
+        <img
+          src="https://res.cloudinary.com/gyang/image/upload/v16843695
+          04/portfolio/quizapp/soundtrap-5Wj_tk8_Ens-unsplash_1_mxccbe.jpg"
+          alt=""
+          class="img-fluid rounded-5"
+        />
+      </div>
+    </div> -->
   </main>
 </template>
 
@@ -27,9 +33,20 @@ import { RouterLink } from "vue-router";
 }
 
 .landing {
-  height: 80vh;
+  /* height: 80vh; */
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.behind {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url("https://res.cloudinary.com/gyang/image/upload/v1684369504/portfolio/quizapp/soundtrap-5Wj_tk8_Ens-unsplash_1_mxccbe.jpg");
+  background-size:cover;
+  /* background-color: black; */
+  /* opacity: 0.5; */
+  height: 80vh;
 }
 </style>
